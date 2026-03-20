84be153cfb0a2f4fabb2b7e87ace7f0be32d7288
Danny G <79025121+dannygme@users.noreply.github.com>
2026-03-21 02:07:58 +0800

    Exp config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ const siteConfig = {
       location: "Taipei, TW",
       dateRange: "Dec 2024 - Present",
       bullets: [
-        "Designed operational readiness and competency standards for aviation, reducinh competency gaps by 80%.",
+        "Reduced aviation readiness gaps by 80% through standardized competency frameworks.",
       ],
     },
     {
